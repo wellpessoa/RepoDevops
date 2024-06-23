8a0ce4614a88adfb5bef5b96904bcc915b3a8d5c
wellpessoa <157530519+wellpessoa@users.noreply.github.com>
2024-06-23 18:27:33 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,13 +1,1 @@
-from flask import Flask
-
-# Cria uma instância do Flask
-app = Flask(__name__)
-
-# Define uma rota para a página inicial
-@app.route('/')
-def hello_world():
-    return 'Olá, mundo!'
-
-# Roda a aplicação Flask
-if __name__ == '__main__':
-    app.run(debug=True)
+print("Hello, World!")
